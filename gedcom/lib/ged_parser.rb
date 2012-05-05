93da47567d50e6fb1ebcb716967fe24d9736b20d
Jonathan Lozinski <jonathan.lozinski@sage.com>
2012-05-05 10:36:10 +0100

    GEDcom parser, has line parser which gets tokens

---
+++ gedcom/lib/ged_parser.rb
@@ -0,0 +1,22 @@
+class GEDParser
+  class LineParser
+    def initialize(line)
+      @line = line
+    end
+
+    def parsed
+      tokens = @line.scan /\s*([^\s]+)\s*([^\s]+)\s*(.*)/
+      level, idortag, value = tokens.first
+      values = {
+        :level => level.to_i,
+        :value => value
+      }
+      if idortag.match /^@.*@$/
+        values[:id] = idortag
+      else
+        values[:tag] = idortag
+      end
+      values
+    end
+  end
+end
