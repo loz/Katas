b23020c9e6c200278e26d689e459f5132c0bf99a
Jonathan Lozinski <jonathan.lozinski@sage.com>
2012-12-03 17:38:55 +0000

    Reject/Accepts to short circuit differences

--- Arigeom/lib/arigeom.rb
+++ Arigeom/lib/arigeom.rb
@@ -13,15 +13,27 @@ class Arigeom
     end
   end
 
+  def geometric_acceptor(set)
+    acceptor(set) do |cur, last|
+      cur.to_f / last.to_f
+    end
+  end
+
+  def arithmetic_acceptor(set)
+    acceptor(set) do |cur, last|
+      cur - last
+    end
+  end
+
   def find_geometric(set)
     combination_detect(set) do |combi|
-      geometric_change(combi)
+      geometric_acceptor(combi)
     end
   end
 
   def find_arithmetic(set)
     combination_detect(set) do |combi|
-      arithmetic_change(combi)
+      arithmetic_acceptor(combi)
     end
   end
 
@@ -31,20 +43,31 @@ class Arigeom
     selected  = []
     set.length.downto(2) do |size|
       set.combination(size).each do |s|
-        changes = yield s
-        if changes.uniq.size == 1
-          return s
-        end
+        accepted = yield s
+        return s if accepted
       end
     end
     nil
   end
 
+  def acceptor(set, &block)
+    first = set[0]
+    prev = set[1]
+    last_change = yield(prev, first)
+    set[2..-1].each do |s|
+      change = yield(s, prev)
+      return unless last_change == change
+      prev = s
+      last_change = change
+    end
+    true
+  end
+
   def differ(set, &block)
-    last = set.first
+    prev = set.first
     set[1..-1].map do |s|
-      change = yield(s, last)
-      last = s
+      change = yield(s, prev)
+      prev = s
       change
     end
   end
